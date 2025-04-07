fc704f0ddcbb09e77be041aa82e32572befce08a
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-04-07 23:17:10 +0300

    solved task 3

---
+++ 03_JS-Syntax-Fundamentals/Tasks/03/excellent-grade.js
@@ -0,0 +1,11 @@
+function logMsgForGrade(grade){
+    if(grade >= 5.50){
+        console.log(`Excellent`);
+    }else{
+        console.log(`Not excellent`);
+    }
+}
+
+logMsgForGrade(5.49);
+logMsgForGrade(5.5000);
+logMsgForGrade(5.51);
