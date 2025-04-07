57d420c64524e54446544334e6c3a7728efb1024
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-04-07 23:11:29 +0300

    solved task 2

---
+++ 03_JS-Syntax-Fundamentals/Tasks/02/student-information.js
@@ -0,0 +1,6 @@
+function printGrade(name, age, grade){
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
+}
+
+printGrade("Pesho", 15, 5.50);
+printGrade("Pesho", 18, 5.55555);
